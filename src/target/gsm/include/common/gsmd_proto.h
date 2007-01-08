e78c92e14f36e136c99c9a94606d0874900467bb
Harald Welte <laforge@openmoko.org>
2007-01-08 12:05:43 +0000

    delete old unused empty file

--- src/target/gsm/include/common/gsmd_proto.h
+++
@@ -1,9 +0,0 @@
-#ifndef _GSMD_PROTO_H
-#define _GSMD_PROTO_H
-
-/* this header file describes the protocol between gsmd and libgsmd 
- * (C) 2006 by Harald Welte <hwelte@hmw-consulting.de> */
-
-
-
-#endif /* _GSMD_PROTO_H */
