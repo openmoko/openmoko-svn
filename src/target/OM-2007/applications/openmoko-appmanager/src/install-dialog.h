8f4b6b29bc4f81c73d586a68dc94155ad0e5917d
caowai_song <caowai_song>
2007-01-22 07:27:46 +0000

    Change the license from "GNU Public License" to "GNU Lesser Public License"

--- src/target/OM-2007/applications/openmoko-appmanager/src/install-dialog.h
+++ src/target/OM-2007/applications/openmoko-appmanager/src/install-dialog.h
@@ -6,13 +6,13 @@
  *  Copyright (C) 2006 First International Computer Inc.
  *
  *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU Public License as published by
+ *  it under the terms of the GNU Lesser Public License as published by
  *  the Free Software Foundation; version 2.1 of the license.
  *
  *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Public License for more details.
+ *  GNU Lesser Public License for more details.
  *
  *  Current Version: $Rev$ ($Date$) [$Author$]
  *
