f98f2b0b0988a5af83900a9d33395ae5a107b357
caowai_song <caowai_song>
2007-01-23 01:01:04 +0000

    Revert the licence to GPL

--- src/target/OM-2007/applications/openmoko-appmanager/src/ipkg_cmd.h
+++ src/target/OM-2007/applications/openmoko-appmanager/src/ipkg_cmd.h
@@ -7,14 +7,14 @@
  * Copyright (C) 2006 FIC-SH
  *
  * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Lesser Public License as
+ * modify it under the terms of the GNU General Public License as
  * published by the Free Software Foundation; either version 2, or (at
  * your option) any later version.
  *
  * This program is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Lesser Public License for more details.
+ * General Public License for more details.
  */
 #ifndef _FIC_IPKG_CMD_H
 #define _FIC_IPKG_CMD_H
