8446e87eac7c777debf9e2a49702d567187bed66
alex_tang <alex_tang@openmoko.com>
2007-02-06 03:13:59 +0000

    CopyRight mistake have been modified

--- src/target/OM-2007/openmoko-libs/libmokoui/moko-menu-box.c
+++ src/target/OM-2007/openmoko-libs/libmokoui/moko-menu-box.c
@@ -1,21 +1,22 @@
 /*
- *  libmokoui -- OpenMoko Application Framework UI Library
- *
- *  Authored by Michael 'Mickey' Lauer <mlauer@vanille-media.de>
- *
- *  Copyright (C) 2006 First International Computer Inc.
- *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU Lesser Public License as published by
- *  the Free Software Foundation; version 2.1 of the license.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Lesser Public License for more details.
- *
- *  Current Version: $Rev$ ($Date$) [$Author$]
+ * libmokoui -- OpenMoko Application Framework UI Library
+ * 
+ * Authored by Michael 'Mickey' Lauer <mlauer@vanille-media.de>
+ * 
+ * Copyright (C) 2006-2007 OpenMoko Inc.
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser Public License as published by
+ * the Free Software Foundation; version 2 of the license.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser Public License for more details.
+ * 
+ * Current Version: $Rev$ ($Date$) [$Author$]
  */
+
 #include "moko-menu-box.h"
 
 #include <gtk/gtkimage.h>
