ff0fa3f7cb91fc67e70ee400c458619e59cf8a3e
jiang_li <jiang_li>
2007-02-07 01:15:02 +0000

    fix a edit bug

--- src/target/OM-2007/applications/openmoko-simplemediaplayer/beep/main.c
+++ src/target/OM-2007/applications/openmoko-simplemediaplayer/beep/main.c
@@ -1091,7 +1091,7 @@ main(gint argc, gchar ** argv)
     playlist_clear();
 //    gchar *audio_path = g_build_filename(g_get_home_dir(), "audio", NULL);
     playlist_add_dir("/music");
-    g_free(audio_path);
+//    g_free(audio_path);
    
     openmoko_mainwin_create();
     
