77d2ed6a584489983eb49fbf4d64972608d495cf
caowai_song <caowai_song>
2007-03-14 01:40:41 +0000

    Because the windows-manager will sure that the application will only run once, remove the unuseful code in the application manager.

--- src/target/OM-2007/applications/openmoko-appmanager/src/appmanager-window.c
+++ src/target/OM-2007/applications/openmoko-appmanager/src/appmanager-window.c
@@ -17,13 +17,6 @@
  *
  *  @author Chaowei Song (songcw@fic-sh.com.cn)
  */
-
-#include <unistd.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <errno.h>
-#include <signal.h>
-
 #include <libmokoui/moko-application.h>
 #include <libmokoui/moko-paned-window.h>
 #include <libmokoui/moko-tool-box.h>
@@ -41,82 +34,6 @@
 #include "package-list.h"
 #include "select-menu.h"
 
-static GtkWidget* mainwindow = NULL;
-
-static void 
-handle_sigusr1 (int value)
-{
-  if (mainwindow == NULL)
-    {
-      return;
-    }
-  gtk_window_present (GTK_WINDOW (mainwindow));
-  signal (SIGUSR1, handle_sigusr1);
-}
-
-static pid_t 
-testlock (char *fname)
-{
-  int fd;
-  struct flock fl;
-
-  fd = open (fname, O_WRONLY, S_IWUSR);
-  if (fd < 0)
-    {
-      if (errno == ENOENT)
-        {
-          return 0;
-        }
-      else
-        {
-          perror ("Test lock open file");
-          return -1;
-        }
-    }
-
-  fl.l_type = F_WRLCK;
-  fl.l_whence = SEEK_SET;
-  fl.l_start = 0;
-  fl.l_len = 0;
-
-  if (fcntl (fd, F_GETLK, &fl) < 0)
-    {
-      close (fd);
-      return -1;
-    }
-  close (fd);
-
-  if (fl.l_type == F_UNLCK)
-    return 0;
-
-  return fl.l_pid;
-}
-
-static void 
-setlock (char *fname)
-{
-  int fd;
-  struct flock fl;
-
-  fd = open (fname, O_WRONLY|O_CREAT, S_IWUSR);
-  if (fd < 0)
-    {
-      perror ("Set lock open file");
-      return ;
-    }
-
-  fl.l_type = F_WRLCK;
-  fl.l_whence = SEEK_SET;
-  fl.l_start = 0;
-  fl.l_len = 0;
-
-  if (fcntl (fd, F_SETLK, &fl) < 0)
-    {
-      perror ("Lock file");
-      close (fd);
-    }
-}
-
 /**
  * @brief The start function.
  */
@@ -133,7 +50,6 @@ main (int argc, char* argv[])
   GtkWidget       *detail;
 
   gint            ret;
-  pid_t           lockapp;
 
   g_debug ("appplication manager start up");
 
@@ -143,13 +59,6 @@ main (int argc, char* argv[])
     {
       /* Add init code. */
     }
-  lockapp = testlock ("/tmp/appmgr.lock");
-  if (lockapp > 0)
-    {
-      kill (lockapp, SIGUSR1);
-      return 0;
-    }
-  setlock ("/tmp/appmgr.lock");
 
   appdata = application_manager_data_new ();
   if (appdata == NULL)
@@ -164,9 +73,6 @@ main (int argc, char* argv[])
   g_signal_connect (G_OBJECT (window), "delete_event", 
                     G_CALLBACK (gtk_main_quit), NULL);
   application_manager_data_set_main_window (appdata, window);
-
-  /* Save the main window */
-  mainwindow = GTK_WIDGET(window);
 
   appmenu = application_menu_new (appdata);
   moko_paned_window_set_application_menu (window, appmenu);
@@ -203,8 +109,6 @@ main (int argc, char* argv[])
     }
 
   package_list_add_section_to_filter_menu (appdata);
-
-  signal (SIGUSR1, handle_sigusr1);
 
   gtk_widget_show_all (GTK_WIDGET (window));
   g_debug ("application manager enter main loop");
