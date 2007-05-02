43ede0fcf22c3c56bf3e12ab7b8725c90617ca93
Thomas Wood <thomas@openedhand.com>
2007-05-02 11:51:00 +0000

    * Makefile.am: * configure.ac: * po/LINGUAS: * po/Makefile.in.in: Add gettext and intltool support
    * libmokoui/Makefile.am:
    * libmokoui/moko-stock.c:
    * libmokoui/moko-stock.h:
    Add moko-stock for adding OpenMoko stock items

---
+++ src/target/OM-2007/openmoko-libs/libmokoui/moko-stock.c
@@ -0,0 +1,47 @@
+#include <gtk/gtk.h>
+#include <moko-stock.h>
+#include <glib/gi18n.h>
+#include <config.h>
+
+
+static const GtkStockItem moko_items [] =
+{
+  { MOKO_STOCK_CALL_HANGUP, N_("Hang Up"), 0, 0, GETTEXT_PACKAGE },
+  { MOKO_STOCK_CALL_HOLD, N_("Hold"), 0, 0, GETTEXT_PACKAGE },
+  { MOKO_STOCK_CALL_REDIAL, N_("Redial"), 0, 0, GETTEXT_PACKAGE },
+  { MOKO_STOCK_CONTACT_ADDRESS, N_("Address"), 0, 0, GETTEXT_PACKAGE },
+  { MOKO_STOCK_CONTACT_DELETE, N_("Delete Contact"), 0, 0, GETTEXT_PACKAGE },
+  { MOKO_STOCK_CONTACT_EMAIL, N_("E-Mail"), 0, 0, GETTEXT_PACKAGE },
+  { MOKO_STOCK_CONTACT_GROUPS, N_("Groups"), 0, 0, GETTEXT_PACKAGE },
+  { MOKO_STOCK_CONTACT_NEW, N_("New Contact"), 0, 0, GETTEXT_PACKAGE },
+  { MOKO_STOCK_CONTACT_PHONE, N_("Phone"), 0, 0, GETTEXT_PACKAGE },
+  { MOKO_STOCK_FOLDER_DELETE, N_("Delete Folder"), 0, 0, GETTEXT_PACKAGE },
+  { MOKO_STOCK_HISTORY, N_("History"), 0, 0, GETTEXT_PACKAGE },
+  { MOKO_STOCK_MAIL_FORWARD, N_("Forward"), 0, 0, GETTEXT_PACKAGE },
+  { MOKO_STOCK_MAIL_MARK_READ, N_("Mark as Read"), 0, 0, GETTEXT_PACKAGE },
+  { MOKO_STOCK_MAIL_NEW, N_("New Mail"), 0, 0, GETTEXT_PACKAGE },
+  { MOKO_STOCK_MAIL_READ, N_("Read Mail"), 0, 0, GETTEXT_PACKAGE },
+  { MOKO_STOCK_MAIL_REPLY_ALL, N_("Reply to All"), 0, 0, GETTEXT_PACKAGE },
+  { MOKO_STOCK_MAIL_REPLY_SENDER, N_("Reply to Sender"), 0, 0, GETTEXT_PACKAGE },
+  { MOKO_STOCK_MAIL_SEND, N_("Send"), 0, 0, GETTEXT_PACKAGE },
+  { MOKO_STOCK_PHONE_BOOK, N_("Phone Book"), 0, 0, GETTEXT_PACKAGE },
+  { MOKO_STOCK_SMS_NEW, N_("New SMS"), 0, 0, GETTEXT_PACKAGE },
+  { MOKO_STOCK_SPEAKER, N_("Speaker"), 0, 0, GETTEXT_PACKAGE },
+  { MOKO_STOCK_VIEW, N_("View"), 0, 0, GETTEXT_PACKAGE },
+};
+
+
+static gboolean registered = FALSE;
+
+void
+moko_register_stock ()
+{
+  /* make sure we never register the icons twice */
+  if (registered)
+    return;
+
+  gtk_stock_add_static (moko_items, G_N_ELEMENTS (moko_items));
+
+
+  registered = TRUE;
+}
---
+++ src/target/OM-2007/openmoko-libs/libmokoui/moko-stock.h
@@ -0,0 +1,37 @@
+
+/*
+ * OpenMoko Stock Items
+ *
+ * Some of these correspond to icon names from the Freedesktop.org Icon Naming
+ * Specification:
+ * http://standards.freedesktop.org/icon-naming-spec/icon-naming-spec-latest.html
+ *
+ * Names that start with "moko-" are currently specific to libmokoui
+ *
+ * The stock items are named by context, then action. Please keep the list in
+ * alphabetical order.
+ *
+ */
+
+#define MOKO_STOCK_CALL_HANGUP "moko-stock-call-hangup"
+#define MOKO_STOCK_CALL_HOLD "moko-stock-call-hold"
+#define MOKO_STOCK_CALL_REDIAL "moko-stock-call-redial"
+#define MOKO_STOCK_CONTACT_ADDRESS "moko-stock-contact-address"
+#define MOKO_STOCK_CONTACT_DELETE "moko-stock-contact-delete"
+#define MOKO_STOCK_CONTACT_EMAIL "moko-stock-contact-email"
+#define MOKO_STOCK_CONTACT_GROUPS "moko-stock-contact-groups"
+#define MOKO_STOCK_CONTACT_NEW "moko-stock-contact-new"
+#define MOKO_STOCK_CONTACT_PHONE "moko-stock-contact-phone"
+#define MOKO_STOCK_FOLDER_DELETE "moko-stock-folder-delete"
+#define MOKO_STOCK_HISTORY "moko-stock-history"
+#define MOKO_STOCK_MAIL_FORWARD "mail-forward"
+#define MOKO_STOCK_MAIL_MARK_READ "mail-mark-read"
+#define MOKO_STOCK_MAIL_NEW "mail-message-new"
+#define MOKO_STOCK_MAIL_READ "mail-read"
+#define MOKO_STOCK_MAIL_REPLY_ALL "mail-reply-all"
+#define MOKO_STOCK_MAIL_REPLY_SENDER "mail-reply-sender"
+#define MOKO_STOCK_MAIL_SEND "mail-send"
+#define MOKO_STOCK_PHONE_BOOK "moko-stock-phone-book"
+#define MOKO_STOCK_SMS_NEW "moko-stock-new-sms"
+#define MOKO_STOCK_SPEAKER "moko-stock-speaker"
+#define MOKO_STOCK_VIEW "moko-stock-view"
