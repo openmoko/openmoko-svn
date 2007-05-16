008790b4f9272252ed2f16349eccd092c2cd95c2
Thomas Wood <thomas@openedhand.com>
2007-05-16 16:00:50 +0000

    openmoko-dialer: fix compiler warnings

--- src/target/OM-2007/applications/openmoko-dialer/src/dialer-window-talking.c
+++ src/target/OM-2007/applications/openmoko-dialer/src/dialer-window-talking.c
@@ -220,7 +220,7 @@ on_window_talking_hide (GtkWidget * widget, MokoDialerData * appdata)
   moko_dialer_textview_empty (appdata->moko_dtmf_text_view);
 
   /* talking window has been hidden, so we should probably make sure the... */
-  moko_gsmd_connection_voice_hangup (data->connection);
+  moko_gsmd_connection_voice_hangup (appdata->connection);
 
 /*  add_histroy_entry (appdata, appdata->g_state.historytype,
                      appdata->g_peer_info.name,
@@ -295,8 +295,8 @@ window_talking_init (MokoDialerData * p_dialer_data)
     GtkWidget *status = NULL;
     GtkWidget *content_talk = NULL;
     GtkWidget *content_dtmf = NULL;
-    MokoFingerToolBox *tools = NULL;
-    MokoFingerWheel *wheel = NULL;
+    GtkWidget *tools = NULL;
+    GtkWidget *wheel = NULL;
     GtkWidget *mokodialerpanel = NULL;
     MokoPixmapButton *button;
     GtkWidget *image;
@@ -390,7 +390,7 @@ window_talking_init (MokoDialerData * p_dialer_data)
     tools = moko_finger_window_get_toolbox (window);
     button =
       MOKO_PIXMAP_BUTTON (moko_finger_tool_box_add_button_without_label
-                          (tools));
+                          (MOKO_FINGER_TOOL_BOX (tools)));
     image = file_new_image_from_relative_path ("speaker.png");
     moko_pixmap_button_set_finger_toolbox_btn_center_image
       (MOKO_PIXMAP_BUTTON (button), image);
@@ -400,22 +400,22 @@ window_talking_init (MokoDialerData * p_dialer_data)
 
     button =
       MOKO_PIXMAP_BUTTON (moko_finger_tool_box_add_button_without_label
-                          (tools));
+                          (MOKO_FINGER_TOOL_BOX (tools)));
     image = file_new_image_from_relative_path ("dtmf.png");
     moko_pixmap_button_set_finger_toolbox_btn_center_image
       (MOKO_PIXMAP_BUTTON (button), image);
     g_signal_connect (G_OBJECT (button), "clicked",
                       G_CALLBACK (cb_tool_button_dtmf_talk_clicked),
                       p_dialer_data);
-    p_dialer_data->buttonTalk_DTMF = button;
+    p_dialer_data->buttonTalk_DTMF = GTK_WIDGET (button);
     p_dialer_data->imageDTMF = image;
     p_dialer_data->imageTALK =
       file_new_image_from_relative_path ("talking.png");
 
 
     button =
-      MOKO_PIXMAP_BUTTON (moko_finger_tool_box_add_button_without_label
-                          (tools));
+      MOKO_PIXMAP_BUTTON (moko_finger_tool_box_add_button_without_label (
+                          MOKO_FINGER_TOOL_BOX (tools)));
     image = file_new_image_from_relative_path ("hangup.png");
     moko_pixmap_button_set_finger_toolbox_btn_center_image
       (MOKO_PIXMAP_BUTTON (button), image);
