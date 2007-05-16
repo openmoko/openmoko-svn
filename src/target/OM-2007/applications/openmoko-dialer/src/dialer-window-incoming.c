cae90b9c72123dd81d7fb82b02cfeae375b3a7ac
Thomas Wood <thomas@openedhand.com>
2007-05-16 15:58:43 +0000

    openmoko-dialer: add missing parameter, fix comiler warnings

--- src/target/OM-2007/applications/openmoko-dialer/src/dialer-window-incoming.c
+++ src/target/OM-2007/applications/openmoko-dialer/src/dialer-window-incoming.c
@@ -229,7 +229,7 @@ window_incoming_show (MokoDialerData *data)
     moko_gsmd_connection_voice_accept (data->connection);
     /* dialer_window_talking_show (data); */
     if (!data->window_talking)
-      window_talking_init ();
+      window_talking_init (data);
     gtk_widget_show_all (data->window_talking);
   }
   else
