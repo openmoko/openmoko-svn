54ef81f7dc84da812678c0398184aab52508d80e
Stefan Schmidt <stefan@openmoko.org>
2007-05-20 16:20:06 +0000

    2007-05-20  Stefan Schmidt <stefan@openmoko.org>
            * src/mokodesktop.c:
            * src/mokodesktop.h:
            * src/mokodesktop_item.c:
            * src/mokodesktop_item.h:
    
    		Add missing licence header.

--- src/target/OM-2007/applications/openmoko-mainmenu/src/mokodesktop_item.h
+++ src/target/OM-2007/applications/openmoko-mainmenu/src/mokodesktop_item.h
@@ -1,3 +1,24 @@
+/**
+ *  @file mokodesktop_item.h
+ *  @brief The Main Menu in the Openmoko
+ *
+ *  Authored by Sun Zhiyong <sunzhiyong@fic-sh.com.cn>
+ *
+ *  Copyright (C) 2006-2007 OpenMoko Inc.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU Public License as published by
+ *  the Free Software Foundation; version 2 of the license.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Public License for more details.
+ *
+ *  Current Version: $Rev$ ($Date$) [$Author$]
+ *
+ */
+
 #ifndef _HAVE_MOKODESKTOP_ITEM_H
 #define _HAVE_MOKODESKTOP_ITEM_H
 
