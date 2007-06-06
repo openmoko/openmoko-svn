88bee4323dd1c29e216996a7dd77689b4ca87d64
Stefan Schmidt <stefan@openmoko.org>
2007-06-06 20:18:34 +0000

    2007-06-06  Stefan Schmidt <stefan@openmoko.org>
            * src/buttonactions.h:
            * src/mokodesktop.c:
            * src/mokodesktop.h:
            * src/stylusmenu.h:
    
            Add proper license header

--- src/target/OM-2007/panel-plugins/openmoko-panel-mainmenu/src/buttonactions.h
+++ src/target/OM-2007/panel-plugins/openmoko-panel-mainmenu/src/buttonactions.h
@@ -1,3 +1,21 @@
+/*
+ *  openmoko-panel-mainmenu: handle action buttons
+ *
+ *  Authored by Michael 'Mickey' Lauer <mlauer@vanille-media.de>
+ *
+ *  Copyright (C) 2007 OpenMoko, Inc.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU Public License as published by
+ *  the Free Software Foundation; version 2.1 of the license.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Public License for more details.
+ *
+ *  Current Version: $Rev$ ($Date$) [$Author$]
+ */
 #ifndef BUTTONACTIONS_H
 #define BUTTONACTIONS_H
 
--- src/target/OM-2007/panel-plugins/openmoko-panel-mainmenu/src/mokodesktop.h
+++ src/target/OM-2007/panel-plugins/openmoko-panel-mainmenu/src/mokodesktop.h
@@ -1,3 +1,22 @@
+/*
+ *  openmoko-mainmenu
+ *
+ *  Authored by Sun Zhiyong <sunzhiyong@fic-sh.com.cn>
+ *
+ *  Copyright (C) 2006-2007 OpenMoko, Inc.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; version 2.0 of the license.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Public License for more details.
+ *
+ *  Current Version: $Rev$ ($Date$) [$Author$]
+ */
+
 #ifndef _MOKODESKTOP_H
 #define _MOKODESKTOP_H
 
