a9f414826d30d1db11b26518fcebc07469bb6e56
Michael Lauer <mickey@openmoko.org>
2007-06-16 15:14:32 +0000

    libmokoui/moko-ui: add convenience function for showing a MokoBanner

--- src/target/OM-2007/openmoko-libs/libmokoui/moko-ui.h
+++ src/target/OM-2007/openmoko-libs/libmokoui/moko-ui.h
@@ -22,6 +22,7 @@
 
 #include "moko-alignment.h"
 #include "moko-application.h"
+#include "moko-banner.h"
 #include "moko-details-window.h"
 #include "moko-dialog.h"
 #include "moko-dialog-window.h"
@@ -40,5 +41,18 @@
 #include "moko-tool-box.h"
 #include "moko-tree-view.h"
 #include "moko-window.h"
+
+void moko_ui_banner_show_text( gint timeout, const gchar* message, ... )
+{
+    va_list a;
+    g_return_if_fail( timeout ); // don't allow permanent banners using the simple interface
+    va_start( a, message );
+    const gchar* string = g_strdup_vprintf( message, a );
+    va_end( a );
+    MokoBanner* banner = moko_banner_new();
+    moko_banner_show_text( banner, string, timeout );
+    g_object_unref( banner );
+    g_free( string );
+}
 
 #endif  /* _MOKO_UI_H_ */
