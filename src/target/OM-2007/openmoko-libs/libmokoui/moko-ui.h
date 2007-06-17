d1bfe30779d12298a6f84d13868496a0557ec40e
Michael Lauer <mickey@openmoko.org>
2007-06-17 04:32:29 +0000

    move...

--- src/target/OM-2007/openmoko-libs/libmokoui/moko-ui.h
+++ src/target/OM-2007/openmoko-libs/libmokoui/moko-ui.h
@@ -42,17 +42,6 @@
 #include "moko-tree-view.h"
 #include "moko-window.h"
 
-void moko_ui_banner_show_text( gint timeout, const gchar* message, ... )
-{
-    va_list a;
-    g_return_if_fail( timeout ); // don't allow permanent banners using the simple interface
-    va_start( a, message );
-    const gchar* string = g_strdup_vprintf( message, a );
-    va_end( a );
-    MokoBanner* banner = moko_banner_new();
-    moko_banner_show_text( banner, string, timeout );
-    g_object_unref( banner );
-    g_free( string );
-}
+void moko_ui_banner_show_text( gint timeout, const gchar* message, ... );
 
 #endif  /* _MOKO_UI_H_ */
