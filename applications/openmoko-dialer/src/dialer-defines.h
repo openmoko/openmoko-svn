058819ce3fcde5cf815a677da5554fd49269314a
Neil J Patel <njp@o-hand.com>
2007-07-13 11:18:36 +0000

    2007-07-13  Neil J. Patel  <njp@o-hand.com>
    	* src/dialer-defines.h:
    	Some global dialer variables.

---
+++ applications/openmoko-dialer/src/dialer-defines.h
@@ -0,0 +1,25 @@
+/*
+ *  dialer-defines.h; Program wide variables.
+ *
+ *  Authored by OpenedHand Ltd <info@openedhand.com>
+ *
+ *  Copyright (C) 2006-2007 OpenMoko Inc.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser Public License as published by
+ *  the Free Software Foundation; version 2 of the license.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser Public License for more details.
+ *
+ *  Current Version: $Rev$ ($Date$) [$Author$]
+ */
+
+#ifndef DIALER_DEFINES_H_
+#define DIALER_DEFINES_H_
+
+#define AUTOCOMPLETE_N_CHARS 4
+
+#endif /* DIALER_DEFINES_H_ */
