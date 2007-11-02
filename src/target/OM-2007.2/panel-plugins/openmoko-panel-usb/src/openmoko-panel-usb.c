86d4d81c265091721a58350d809afe7f822e98d5
Michael Lauer <mickey@openmoko.org>
2007-11-02 08:19:21 +0000

    openmoko-panel-usb: remove usb code, add dbus listener

--- src/target/OM-2007.2/panel-plugins/openmoko-panel-usb/src/openmoko-panel-usb.c
+++ src/target/OM-2007.2/panel-plugins/openmoko-panel-usb/src/openmoko-panel-usb.c
@@ -15,10 +15,7 @@
  */
 #include <libmokopanelui2/moko-panel-applet.h>
 
-#include <sys/types.h>
-#include <linux/limits.h>
-#include <usb.h>
-
+#include <dbus/dbus-glib.h>
 #include <gtk/gtkimage.h>
 #include <time.h>
 
@@ -33,120 +30,39 @@ usb_applet_free (UsbApplet *applet)
     g_slice_free (UsbApplet, applet);
 }
 
-void print_endpoint(struct usb_endpoint_descriptor *endpoint)
+static void usb_applet_dbus_signal( void* data )
 {
-    printf(" bEndpointAddress: %02xh\n", endpoint->bEndpointAddress);
-    printf(" bmAttributes: %02xh\n", endpoint->bmAttributes);
-    printf(" wMaxPacketSize: %d\n", endpoint->wMaxPacketSize);
-    printf(" bInterval: %d\n", endpoint->bInterval);
-    printf(" bRefresh: %d\n", endpoint->bRefresh);
-    printf(" bSynchAddress: %d\n", endpoint->bSynchAddress);
+    g_debug( "usb_applet_dbus_signal: received signal. data pointer = %p", data );
 }
 
-void print_altsetting(struct usb_interface_descriptor *interface)
+#define USB_DBUS_SERVICE      "com.burtonini"
+#define USB_DBUS_PATH         "/com/burtonini"
+#define USB_DBUS_INTERFACE    "com.burtonini"
+
+static void usb_applet_init_dbus( UsbApplet* applet )
 {
-    int i;
+    GError* error = NULL;
+    DBusGConnection* bus = dbus_g_bus_get( DBUS_BUS_SESSION, &error );
 
-    printf(" bInterfaceNumber: %d\n", interface->bInterfaceNumber);
-    printf(" bAlternateSetting: %d\n", interface->bAlternateSetting);
-    printf(" bNumEndpoints: %d\n", interface->bNumEndpoints);
-    printf(" bInterfaceClass: %d\n", interface->bInterfaceClass);
-    printf(" bInterfaceSubClass: %d\n", interface->bInterfaceSubClass);
-    printf(" bInterfaceProtocol: %d\n", interface->bInterfaceProtocol);
-    printf(" iInterface: %d\n", interface->iInterface);
-
-    for (i = 0; i < interface->bNumEndpoints; i++)
-        print_endpoint(&interface->endpoint[i]);
-}
-
-void print_interface(struct usb_interface *interface)
-{
-    int i;
-
-    for (i = 0; i < interface->num_altsetting; i++)
-        print_altsetting(&interface->altsetting[i]);
-}
-
-void print_configuration(struct usb_config_descriptor *config)
-{
-    int i;
-
-    printf(" wTotalLength: %d\n", config->wTotalLength);
-    printf(" bNumInterfaces: %d\n", config->bNumInterfaces);
-    printf(" bConfigurationValue: %d\n", config->bConfigurationValue);
-    printf(" iConfiguration: %d\n", config->iConfiguration);
-    printf(" bmAttributes: %02xh\n", config->bmAttributes);
-    printf(" MaxPower: %d\n", config->MaxPower);
-
-    for (i = 0; i < config->bNumInterfaces; i++)
-        print_interface(&config->interface[i]);
-}
-
-static void usb_applet_dump_usb_status()
-{
-    struct usb_bus *bus;
-    struct usb_device *dev;
-    printf("bus/device idVendor/idProduct\n");
-
-    for (bus = usb_busses; bus; bus = bus->next) {
-        for (dev = bus->devices; dev; dev = dev->next) {
-            int ret, i;
-            char string[256];
-            usb_dev_handle *udev;
-
-            printf("%s/%s %04X/%04X\n", bus->dirname, dev->filename,
-                dev->descriptor.idVendor, dev->descriptor.idProduct);
-
-            udev = usb_open(dev);
-            if (udev) {
-                if (dev->descriptor.iManufacturer) {
-                    ret = usb_get_string_simple(udev, dev->descriptor.iManufacturer, string, sizeof(string));
-                    if (ret > 0)
-                        printf("- Manufacturer : %s\n", string);
-                    else
-                        printf("- Unable to fetch manufacturer string\n");
-                }
-
-                if (dev->descriptor.iProduct) {
-                    ret = usb_get_string_simple(udev, dev->descriptor.iProduct, string, sizeof(string));
-                    if (ret > 0)
-                        printf("- Product : %s\n", string);
-                    else
-                        printf("- Unable to fetch product string\n");
-                }
-
-                if (dev->descriptor.iSerialNumber) {
-                    ret = usb_get_string_simple(udev, dev->descriptor.iSerialNumber, string, sizeof(string));
-                    if (ret > 0)
-                        printf("- Serial Number: %s\n", string);
-                    else
-                        printf("- Unable to fetch serial number string\n");
-                }
-
-                usb_close (udev);
-            }
-
-            if (!dev->config) {
-                printf(" Couldn't retrieve descriptors\n");
-                continue;
-            }
-
-            for (i = 0; i < dev->descriptor.bNumConfigurations; i++)
-                print_configuration(&dev->config[i]);
-        }
+    if (error)
+    {
+        g_warning( "%s: Error acquiring session bus: %s", G_STRLOC, error->message );
+        return;
     }
+
+    DBusGProxy* usb_control_interface = dbus_g_proxy_new_for_name( bus, USB_DBUS_SERVICE, USB_DBUS_PATH, USB_DBUS_INTERFACE );
+    if ( !usb_control_interface )
+    {
+        g_warning( "Could not connect to USB dbus service" );
+        return;
+    }
+
+    dbus_g_proxy_add_signal( usb_control_interface, "SignalTest", G_TYPE_INVALID );
+    dbus_g_proxy_connect_signal( usb_control_interface, "SignalTest", G_CALLBACK(usb_applet_dbus_signal), NULL, NULL );
 }
 
-static void usb_applet_update_status( UsbApplet *applet )
+static void usb_applet_update_status( UsbApplet* applet )
 {
-    usb_init();
-    int new_busses = usb_find_busses();
-    g_debug( "usb_applet_update_status: %d new USBes found", new_busses );
-    int new_devices = usb_find_devices();
-    g_debug( "usb_applet_update_status: %d new USB devices found", new_devices );
-
-    usb_applet_dump_usb_status();
-
     moko_panel_applet_set_icon( applet->mpa, PKGDATADIR "/Usb.png" );
 
 }
@@ -163,6 +79,7 @@ mb_panel_applet_create(const char* id, GtkOrientation orientation)
     applet = g_slice_new( UsbApplet );
     applet->mpa = mokoapplet;
 
+    usb_applet_init_dbus( applet );
     usb_applet_update_status( applet );
 
     gtk_widget_show_all( GTK_WIDGET(mokoapplet) );
