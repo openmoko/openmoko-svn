f2ba884fdf24944053b8f4d7ce7dd2efc85702a0
chris <chris>
2008-01-29 12:54:48 +0000

    *Really* unbreak python bindings

--- src/target/OM-2007.2/libraries/libmokoui2/bindings/python/mokouimodule.c
+++ src/target/OM-2007.2/libraries/libmokoui2/bindings/python/mokouimodule.c
@@ -1,21 +1,21 @@
 #include <pygobject.h>
  
-void moko_register_classes (PyObject *d); 
-extern PyMethodDef moko_functions[];
+void mokoui_register_classes (PyObject *d); 
+extern PyMethodDef mokoui_functions[];
  
 DL_EXPORT(void)
-initmoko(void)
+initmokoui(void)
 {
     PyObject *m, *d;
  
     init_pygobject ();
  
-    m = Py_InitModule ("moko", moko_functions);
+    m = Py_InitModule ("mokoui", mokoui_functions);
     d = PyModule_GetDict (m);
  
-    moko_register_classes (d);
+    mokoui_register_classes (d);
  
     if (PyErr_Occurred ()) {
-        Py_FatalError ("can't initialise module trayicon");
+        Py_FatalError ("can't initialise module mokoui");
     }
 }
