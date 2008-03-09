77835af882592fe1b1388bfaca4b1cdfed9f2388
Erin Yueh <erin_yueh@openmoko.org>
2008-03-09 04:50:55 +0000

    openmoko-messages2: fix the category filter for bug#1246 (Erin Yueh)

--- src/target/OM-2007.2/applications/openmoko-messages2/src/sms-notes.c
+++ src/target/OM-2007.2/applications/openmoko-messages2/src/sms-notes.c
@@ -754,7 +754,7 @@ gboolean notes_visible_func (GtkTreeModel *model, GtkTreeIter *iter,
 			moko_search_bar_get_combo_box (MOKO_SEARCH_BAR (
 				data->notes_search))); 
 		
-		if ((type <= ALL_NOTES) || (!data->author_uid)) return TRUE;
+		if ((type <= ALL_NOTES)/* || (!data->author_uid)*/) return TRUE;
 		
 		gtk_tree_model_get (model, iter,
 			JANA_GTK_NOTE_STORE_COL_CATEGORIES, &categories, -1);
