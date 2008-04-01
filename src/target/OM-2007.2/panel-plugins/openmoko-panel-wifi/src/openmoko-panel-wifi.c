f6b959182bdb3bb6cac7a5d12955d3d68effc3be
rob <rob@openmoko.org>
2008-04-01 10:06:07 +0000

    2008-04-01  Rob Bradford  <rob@openedhand.com>
    	* src/openmoko-panel-wifi.c: (wifi_applet_is_wireless_available),
    	(wifi_applet_update_visibility), (wifi_applet_weak_notify_cb),
    	(wifi_applet_timeout_cb), (mb_panel_applet_create):
    	Poll every 5 seconds for the state of the wifi radio. It would be
    	nicer to be notified rather than needing to poll. This uses the 
    	SIOCGIWTXPOW ioctl().

--- src/target/OM-2007.2/panel-plugins/openmoko-panel-wifi/src/openmoko-panel-wifi.c
+++ src/target/OM-2007.2/panel-plugins/openmoko-panel-wifi/src/openmoko-panel-wifi.c
@@ -16,36 +16,119 @@
  *
  */
 
+#include <sys/types.h>
+#include <sys/ioctl.h>
+#include <stdio.h>
+#include <math.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <netdb.h>		/* gethostbyname, getnetbyname */
+#include <net/ethernet.h>	/* struct ether_addr */
+#include <sys/time.h>		/* struct timeval */
+#include <unistd.h>
+
+#include <linux/if.h>
+#include <linux/wireless.h>
+
 #include <libmokopanelui2/moko-panel-applet.h>
 #include <gtk/gtk.h>
 
-static gboolean
-wifi_applet_is_wireless_available ()
+#define IFACE "eth0"
+#define QUERY_FREQ 5 
+
+typedef struct 
 {
-  /* Not implemented yet */
-  return TRUE;
+  GtkWidget *applet;
+  int sock; /* socket for doing query on */
+  gchar *iface;
+  guint timeout_id;
+} WifiAppletData;
+
+static gboolean
+wifi_applet_is_wireless_available (WifiAppletData *applet)
+{
+  struct iwreq wrq;
+
+  /* Clear our request and set the interface name */
+  memset (&wrq, 0, sizeof (struct iwreq));
+  strncpy ((char *)&wrq.ifr_name, applet->iface, IFNAMSIZ);
+
+  /* Feel the power, uhh, do the ioctl() */
+  if (ioctl (applet->sock, SIOCGIWTXPOW, &wrq) != 0)
+  {
+    g_warning ("Error performing ioctl: %s", g_strerror (errno));
+    return FALSE;
+  }
+
+  return !wrq.u.txpower.disabled;
 }
 
 static void
-wifi_applet_update_visibility (GtkWidget *applet)
+wifi_applet_update_visibility (WifiAppletData *applet)
 {
-  if (wifi_applet_is_wireless_available ())
-    gtk_widget_show_all (applet);
+  if (wifi_applet_is_wireless_available (applet))
+    gtk_widget_show_all (applet->applet);
   else
-    gtk_widget_hide_all (applet);
+    gtk_widget_hide_all (applet->applet);
+}
+
+/* Tidy up callback. Don't want stray timeouts. */
+static void
+wifi_applet_weak_notify_cb (gpointer data, GObject *dead_object)
+{
+  WifiAppletData *applet = (WifiAppletData *)data;
+
+  g_source_remove (applet->timeout_id);
+  close (applet->sock);
+  g_free (applet);
+}
+
+static gboolean
+wifi_applet_timeout_cb (gpointer data)
+{
+  wifi_applet_update_visibility ((WifiAppletData *)data);
+
+  return TRUE;
 }
 
 G_MODULE_EXPORT GtkWidget * 
 mb_panel_applet_create (const gchar *id, GtkOrientation orientation)
 {
-    GtkWidget *applet;
+  WifiAppletData *applet;
 
-    applet = moko_panel_applet_new ();
+  applet = g_new0 (WifiAppletData, 1);
 
-    moko_panel_applet_set_icon (MOKO_PANEL_APPLET (applet),
-        PKGDATADIR "/" "Wifi.png");
+  /* Open socket to perform ioctl() on */
+  applet->sock = socket (AF_INET, SOCK_DGRAM, 0);
 
-    wifi_applet_update_visibility (applet);
+  if (!applet->sock)
+  {
+    g_warning ("Unable to open socket: %s", g_strerror (errno));
+    return FALSE;
+  }
 
-    return applet;
+  /* Our interface name */
+  applet->iface = IFACE;
+
+  applet->applet = moko_panel_applet_new ();
+
+  /* 
+   * Weak reference so we can find out when the applet is destroyed to get
+   * rid of the timeout and tidy up
+   */
+  g_object_weak_ref ((GObject *)applet->applet, wifi_applet_weak_notify_cb, applet);
+
+  moko_panel_applet_set_icon (MOKO_PANEL_APPLET (applet->applet),
+      PKGDATADIR "/" "Wifi.png");
+
+  wifi_applet_update_visibility (applet);
+
+  applet->timeout_id = g_timeout_add_seconds (QUERY_FREQ, wifi_applet_timeout_cb, 
+      applet);
+
+  return applet->applet;
 }
