8a8ae65a5fadc97a271e5e8f6dc716f90383ead5
Marek Lindner <marek@openmoko.org>
2008-09-25 03:37:48 +0000

    add missing files  :|

---
+++ src/host/pye17/ecore_evas/ecore_evasmodule.c
@@ -0,0 +1,22 @@
+#include <pygobject.h>
+
+void ecore_evas_register_classes(PyObject *d);
+extern PyMethodDef ecore_evas_functions[];
+
+DL_EXPORT(void)
+initecore_evas(void)
+{
+  PyObject *m, *d;
+
+  init_pygobject();
+
+  m = Py_InitModule("ecore_evas", ecore_evas_functions);
+
+  d = PyModule_GetDict(m);
+
+  ecore_evas_register_classes(d);
+
+  if (PyErr_Occurred()) {
+    Py_FatalError ("can't initialise module ecore_evas");
+  }
+}
