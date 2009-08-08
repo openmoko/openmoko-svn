f2a1bb37b41ec278ccad898e5cc3cd24afe29696
Werner Almesberger <werner@openmoko.org>
2009-08-08 19:35:16 +0000

    - moved handling of frame area from gui.c to gui_frame.c and cleaned up their   includes

---
+++ eda/fped/gui_frame.h
@@ -0,0 +1,26 @@
+/*
+ * gui_frame.h - GUI, frame window
+ *
+ * Written 2009 by Werner Almesberger
+ * Copyright 2009 by Werner Almesberger
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ */
+
+
+#ifndef GUI_FRAME_H
+#define GUI_FRAME_H
+
+#include <gtk/gtk.h>
+
+
+void make_popups(void);
+
+void select_frame(struct frame *frame);
+
+void build_frames(GtkWidget *vbox);
+
+#endif /* !GUI_FRAME_H */
