e98a6824cdc16108b0a5937cd87065f5d4032dda
Werner Almesberger <werner@openmoko.org>
2010-02-08 03:47:47 +0000

    Follow "OldPn" references in descriptions.
    - eda/boom/misc.pl: moved description lookup from "prettyord" and "annotate"
    - eda/boom/misc.pl: added mechanism to translate pointers in descriptions back
      to component references within a lookup
    - eda/boom/prettyord, eda/boom/annotate: new hackish option -s/from/to/ to add
      translations
    - bom/Makefile: new target "show-fic" to show the list of items from FIC's
      inventory
    - bom/Makefile: use -s for "show-fic" and annotation to handle "OldPn" records
    - bom/Makefile: made regexps used to grep through the order list more robust

--- eda/boom/misc.pl
+++ eda/boom/misc.pl
@@ -23,4 +23,57 @@ sub eq
 }
 
 
+#
+# When looking for a description, we also consider equivalent parts.
+#
+# Furthermore, some descriptions may just be pointers to other descriptions.
+# Users can add regular expressions that are used to extract references from
+# a description, which are then looked up as well.
+#
+
+sub __dsc_lookup
+{
+    local ($id) = @_;
+
+    for ($id, &eq($id)) {
+        return $dsc{$_} if defined $dsc{$_};
+    }
+    return undef;
+}
+
+
+sub dsc_find
+{
+    my $id = $_[0];
+    LOOKUP: while (1) {
+	my $dsc = &__dsc_lookup($id);
+	return undef unless defined $dsc;
+	for (my $i = 0; $i <= $#xlat_from; $i++) {
+# @@@ this is UUUUHHHGLLEEEEE !!! Why can't I just expand $to[$i] ?
+	    next
+	      unless ($id = $dsc) =~ s/^.*$xlat_from[$i].*$/$xlat_to[$i] $1/;
+	    next LOOKUP if defined &__dsc_lookup($id);
+	}
+	return $dsc;
+    }
+    return undef;
+}
+
+
+sub dsc_xlat
+{
+    local ($from, $to) = @_;
+    push(@xlat_from, $from);
+    push(@xlat_to, $to);
+}
+
+
+sub dsc_xlat_arg
+{
+    return undef unless $_[0] =~ /^(.)([^\1]*)\1([^\1]*)\1$/;
+    &dsc_xlat($2, $3);
+    return 1;
+}
+
+
 return 1;
