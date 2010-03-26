14bbbd169860e3f02c8c2b501bb275a1766617df
Werner Almesberger <werner@openmoko.org>
2010-03-26 07:40:41 +0000

    Automated updating of Digi-Key's inventory and price list.
    - bom/dk/dk-db.pl, bom/dk/Makefile: we can now look up the parts listed in
      digi-key.equ in Digi-Key's database and put pricing, stock, and descriptions
      into digi-key.inv and digi-key.dsc
    - bom/Makefile: added dependencies for generated files in bom/dk/
    - bom/dk/digi-key.inv, bom/dk/digi-key.dsc: removed from SVN because they're 
      now generated

---
+++ gta02-core/bom/dk/dk-db.pl
@@ -0,0 +1,95 @@
+#!/usr/bin/perl
+
+sub rows
+{
+    local $s = $_[0];
+    my @res = ();
+
+    while ($s =~ m#.*?<tr>(.*?)</tr>#) {
+	push(@res, $1);
+	$s = $';
+    }
+    return @res;
+}
+
+
+sub cols
+{
+    local $s = $_[0];
+    my @res = ();
+
+    while ($s =~ m#.*?<td[^>]*>(.*?)</td>#) {
+	push(@res, $1);
+	$s = $';
+    }
+    return @res;
+}
+
+
+sub usage
+{
+    print STDERR "usage: $0 (query|dsc|inv) [file ...]\n";
+    exit(1);
+}
+
+
+$mode = shift @ARGV;
+&usage unless $mode eq "query" || $mode eq "dsc" || $mode eq "inv";
+
+if ($mode eq "query") {
+    while (<>) {
+	chop;
+	s/#.*//;
+	next if /^\s*$/;
+	next if /^\s/;
+	s/\s.*//;
+	push(@pn, $_);
+    }
+
+    exit unless 0+@pn;
+
+    $cmd = "wget -nv -O - ".join(" ",
+      map
+      "http://search.digikey.com/scripts/DkSearch/dksus.dll?Detail\\&name=$_",
+      @pn);
+    $q = `$cmd`;
+
+    print $q;
+    exit;
+}
+
+
+$q = join("", <>);
+$q =~ tr/\r\n//d;
+
+print "#DSC\n" if $mode eq "dsc";
+print "#INV\n" if $mode eq "inv";
+print "# MACHINE-GENERATED. DO NOT EDIT !\n";
+print "# ", `date -u`;
+
+for (split(/<!DOCTYPE HTML/, $q)) {
+    next unless m#align=right>Digi-Key Part Number</th><td>([^<]+)</td#;
+    $pn = $1;
+    $qty = 0;
+    if (m#align=right>Quantity Available</th><td[^>]*>([0-9,]+)<#) {
+	($qty = $1) =~ tr/,//d;
+    }
+    next unless m#align=right>Description</th><td>(.*?)</td#;
+    $dsc = $1;
+    next unless m#<table.*<th>Price Break<(.*?)</table>#;
+    if ($mode eq "dsc") {
+	print "DIGI-KEY $pn $dsc\n";
+	next;
+    }
+    print "DIGI-KEY $pn $qty USD";
+    for (&rows($1)) {
+	@c = &cols($_);
+	next unless $c[0] =~ /^[0-9,]+$/;
+	next unless $c[1] =~ /^[0-9.]+$/;
+	$c[0] =~ tr/,//d;
+	$c[1] =~ tr/,//d;	# let's hope we don't need this one often :)
+	$c[1] =~ s/0+$// if $c[1] =~ /\./;
+	print " $c[0] $c[1]";
+    }
+    print "\n";
+}
